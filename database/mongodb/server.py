bedd53e9275d22073fd92fd21d422492c90f4cf4
berrytern <berrytern@gmail.com>
2024-09-16 23:49:04 -0300

    chore: grammar adjustments

--- database/mongodb/server.py
+++ database/mongodb/server.py
@@ -3,45 +3,45 @@ from pymongo import MongoClient
 # tutorial link: https://pymongo.readthedocs.io/en/stable/tutorial.html
 # examples: https://pymongo.readthedocs.io/en/stable/examples/index.html
 
-host = 'localhost' # we will use localhost as host beacause we are connecting to mongodb server that is running using our host port
-port = 27017 # the default port of mongodb service is 27017
+host = 'localhost' # we will use localhost as host because we are connecting to MongoDB server that is running using our host port
+port = 27017 # the default port of MongoDB service is 27017
 username = "admin" # default username is admin
 password = "admin" # default password is admin
 database = "petshop" # database name
 
 # you can pass connection parameters as kwargs
-# client = MongoClient(host=host, port=port, username=username, password=password) # establish connection with mongodb server
+# client = MongoClient(host=host, port=port, username=username, password=password) # establish connection with MongoDB server
 # or you can pass connection parameters as URI
-client = MongoClient(f"mongodb://{username}:{password}@{host}:{port}/") # establish connection with mongodb server
+client = MongoClient(f"mongodb://{username}:{password}@{host}:{port}/") # establish connection with MongoDB server
 
-print(list(client.list_databases())) # list databases of mongodb server giving the name, size of each one
-print(client.list_database_names()) # list databases names of mongodb server
+print(list(client.list_databases())) # list databases of MongoDB server giving the name, size of each one
+print(client.list_database_names()) # list databases names of MongoDB server
 
-db = client[database] # prepare to create database, but only creates when receive a create command to store data
+db = client[database] # Select the database. It will be created only when data is first stored.
 
-# prepare to create collections, but only creates when receive a create command to store data
+# Prepare collection references. Collections are created only when data is first inserted.
 pet_collection = db["pets"]
 store_collection = db["store"]
 
 # collection.find() returns all documents in the collection
 print("stores: ", list(store_collection.find()))
 
-# observe that no database or collection was created
+# Observe that no database or collection was created
 print(client.list_database_names())
 
 # now we will send command to collection to store new documents
 store = {"name":"petville"}
-# docs of insertedResult type: https://pymongo.readthedocs.io/en/stable/api/pymongo/results.html#pymongo.results.InsertOneResult
+# Docs for InsertOneResult type: https://pymongo.readthedocs.io/en/stable/api/pymongo/results.html#pymongo.results.InsertOneResult
 insertResult = store_collection.insert_one(store)
 
 # now we can check if the document has been created successfully
 # first way is check the return of insert_one method
 print("inserted: ", insertResult.acknowledged)
-# second way is check if the document is present on the collection passing filters as parameters
+# Second way is check if the document is present on the collection passing filters as parameters
 print("stores: ", list(store_collection.find({"name":"petville"})))
 
-# when some document is created, its add an id to the original variable that holds the information
-# now store variable has the _id field added as result of insert_one operation
+# When some document is created, its add an id to the original variable that holds the information
+# now store variable has the _id field added as a result of insert_one operation
 print(store["_id"])
 
 
@@ -50,12 +50,12 @@ print(client.list_database_names())
 print(db.list_collection_names())
 
 
-# there are some validations that can be added to collection
-# unique field validation
+# Validations can be added to collections
+# Create a unique index on the 'name' field
 store_collection.create_index("name", unique=True)
 # 
 
-# try to insert a document with the same field value of already existing document
+# Attempt to insert a document with a 'name' value that already exists
 # raises DuplicateKeyError
 from pymongo.errors import DuplicateKeyError
 try:
@@ -74,9 +74,9 @@ except DuplicateKeyError:
 # store_collection.delete_many(filter) # delete all documents that match with the passed filter
 
 # challenge
-# create some petshot store document and do CRUD operations with this document
-# create three pets that has a field as foreign_key with reference to the petshot store
-# find all pets by petshop foreing_key filter
+# create some pet shop store document and do CRUD operations with this document
+# create three pets that has a field as foreign_key with reference to the pet shop store
+# find all pets by petshop foreign_key filter
 
 # drop collection
 store_collection.drop()
